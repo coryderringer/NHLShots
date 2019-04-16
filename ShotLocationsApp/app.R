306d664bddff04628676cdd5faa18468f5569a75
Cory Derringer <coryderringer@gmail.com>
2019-04-16 14:08:37 -0400

    readability
    Small readability adjustments

--- ShotLocationsApp/app.R
+++ ShotLocationsApp/app.R
@@ -1,13 +1,5 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
 
-
+# import required packages
 library(shiny)
 library(tidyverse)
 library(ggforce)
@@ -18,9 +10,7 @@ library(hexbin)
 ui <- fluidPage(
     
     fluidRow(column(3),
-             column(6,
-        h1("NHL Shot Location Data"),
-            h3("an ongoing endeavor...")),
+             column(6, h1("NHL Shot Location Data"), h3("an ongoing endeavor...")),
         column(3)),
     
     sidebarPanel(
@@ -40,64 +30,14 @@ ui <- fluidPage(
     mainPanel(
         plotOutput(outputId = "shotMap")    
     )
-    
-     
-    # output data:
-    # fluidRow(
-    #     column(12,
-    #         tableOutput(outputId = 'dataTable')
-    #     )
-    # )
+
     
 )
 
 
 # Define server logic required to draw the plot
 server <- function(input, output) {
-    # how to get the inputs into the outputs
-    # 1. save objects as output$title (e.g., output$hist or output$shotMap)
-    # 2. What you save into output should be built with a render function (e.g., renderPlot or renderDataTable)
-    # 3. Use input values to make your outputs (e.g., input$button, input$team)
-   
-    # pd <- reactive({
-    #     # read team csv
-    #     if(input$result != 'Goal'){ # will need this either way
-    #         saves <- read.csv(paste('Datasets/', input$team, 'save.csv', sep = ''))
-    #     }
-    #     if(input$result != 'Save'){
-    #         goals <- read.csv(paste('Datasets/', input$team, 'goal.csv', sep = ''))
-    #     }
-    #     
-    #     d <- read.csv(paste(input$team, '.csv', sep = ''))
-    #     # print(head(d))
-    #     r <- input$result
-    #     
-    #     
-    #     if(r == 'Goal'){
-    #         pd <- filter(d, result == 1)
-    #     }else if(r == 'Save'){
-    #         pd <- filter(d, result == 0)
-    #     }else if(r == 'All'){
-    #         pd <- d
-    #     }
-    #     
-    #     # don't give this option        
-    #     # if(t != 'All'){
-    #     #     # print('FILTER THE DATA!!!')
-    #     #     # print(paste('Only from ', t, sep=''))
-    #     #     pd <- filter(pd, team == t)
-    #     #     # print(head(pd))
-    #     # }
-    # })
     
-    # output$dataTable <- renderTable({
-    #     
-    #     
-    #     pd()
-    #     # print(result == 'Goal')
-    #     # print(team == 'PIT')
-    #     # print(head(pd))
-    # })
     
     output$shotMap <- renderPlot({
     
@@ -110,8 +50,7 @@ server <- function(input, output) {
         
         baseplot <- ggplot(goals, aes(x = x_coord, y = y_coord_flip)) + theme_no_axes() +
 
-            # lines
-
+            # Draw a hockey rink:
             geom_vline(colour = 'black', xintercept = 100) +
             geom_hline(colour = 'black', yintercept = -43)+
             geom_hline(colour = 'black', yintercept = 43) +
@@ -135,13 +74,12 @@ server <- function(input, output) {
             # goal
             geom_rect(xmin = 89, xmax = 92.5, ymin = -3, ymax = 3) +
 
-
             coord_fixed(ylim = c(-42.5, 42.5), xlim = c(0, 100)) +
             labs(title = paste('Team: ', input$team, sep = ''))+
             theme(legend.position = "none")
 
             # actual data
-            # if we need saves:
+            # two different datasets, so we only have to handle the data we're visualizing
             if(input$result == 'Goal'){
                 p <- baseplot + geom_jitter(alpha = .5, height = 1, width = .1, colour = 'red') +
                 labs(subtitle = 'Goals')
